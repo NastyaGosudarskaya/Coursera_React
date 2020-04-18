527768b564627683558c52c0409b370297ea284e
Nastya <gosudarskaya2000@gmail.com>
2020-04-18 14:56:22 +0300

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,7 +26,7 @@ class CommentForm extends Component{
 
     handleSubmit(values){
         console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -77,9 +77,9 @@ class CommentForm extends Component{
                     <Label htmlFor="message">Comment</Label>
                     <Row className="form-group">
                         <Col>
-                            <Control.textarea model=".message" 
-                            id="message" 
-                            name="message" 
+                            <Control.textarea model=".comment" 
+                            id="comment" 
+                            name="comment" 
                             rows="6" 
                             className="form-control"/>
                         </Col>
@@ -120,7 +120,7 @@ function RenderDish({dish}){
         }
 }
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}){
         if (comments!=null){
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -135,7 +135,7 @@ function RenderDish({dish}){
                                 </div>
                             );
                         })}
-                        <CommentForm />
+                        <CommentForm dishId={dishId} addComment={addComment} />
                 </div>
             );
         }
@@ -168,7 +168,10 @@ function RenderDish({dish}){
                     </div>
                     <div className="row">
                         <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}
+                        />
                     </div>
                 </div>
             );
